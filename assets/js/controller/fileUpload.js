c89f260480bd256cbae543390cbf220f7e7ab15f
Fabio Batz <fbatz.mmt-m2014@fh-salzburg.ac.at>
2015-04-13 23:11:33 +0200

    added/refactored ng-file-drop and ng-file-select

--- assets/js/controller/fileUpload.js
+++ assets/js/controller/fileUpload.js
@@ -1,3 +1,39 @@
-angular.module('vidatio').controller('FileUploadCtrl', function ($scope, $upload) {
+angular.module('vidatio').controller('FileUploadCtrl', function ($scope, $http, FileReader) {
 
+    $scope.getFile = function () {
+        $scope.progress = 0;
+        FileReader.readAsDataUrl($scope.file, $scope)
+                      .then(function(result) {
+                          $scope.content = result;
+                      });
+    };
+ 
+    $scope.$on("fileProgress", function(e, progress) {
+        $scope.progress = progress.loaded / progress.total;
+    });
+
+    $scope.getUrl = function() {
+      var url = "http://www.wolfsberg.at/fileadmin/user_upload/Downloads/Haushalt2015.csv";
+
+      $http.jsonp(url)
+          .success(function(data){
+              console.log(data.found);
+          });
+      // // Simple GET request example :
+      // $http.get('http://www.wolfsberg.at/fileadmin/user_upload/Downloads/Haushalt2015.csv').
+      //   success(function(data, status, headers, config) {
+      //     // this callback will be called asynchronously
+      //     // when the response is available
+      //     console.log("success url");
+      //     console.log(data);
+      //     console.log(status);          
+      //   }).
+      //   error(function(data, status, headers, config) {
+      //     // called asynchronously if an error occurs
+      //     // or server returns response with an error status.
+      //     console.log("error url");
+
+      //   });
+    };
+ 
 });
