b8154f636bbad7c41473eeb94fff2a0738f8c599
Lukas Wanko <lwanko.mmt-m2014@fh-salzburg.ac.at>
2015-05-04 22:08:03 +0200

    working on hover effects

--- assets/js/directives/ngFileDrop.js
+++ assets/js/directives/ngFileDrop.js
@@ -1,30 +1,41 @@
-angular.module('vidatio').directive("ngFileDrop",function(){
+angular.module('vidatio').directive("ngFileDrop", function () {
 
 
   return {
-    link: function($scope,el){
-    
-      var preventDefault = function(e){
-      	e.preventDefault();
-      	return false;
-      };
+    link: function ($scope, el) {
+      //Drag-And-Drop Events:
+      //leave: cursor moves out of drop-zone
+      //over: cursor currently moves over the drop-zone
+      //enter: cursor enters the drop-zone
+      //drop: file got dropped on the drop-zone
+      //drag, drag start and drag end isn't needed, because its only for DOM elements
 
-      el.bind("drag", preventDefault);	
-      el.bind("dragstart", preventDefault);
-      el.bind("dragend", preventDefault);
-      el.bind("dragover", preventDefault);
-      el.bind("dragenter", preventDefault);
-      el.bind("dragleave", preventDefault);
-      
-      el.bind("drop", function(e){
-      	e.preventDefault();
+      el.bind("dragover", function (e) {
+        this.classList.add("drag-enter");
+        e.preventDefault();
+        return false;
+      });
+      el.bind("dragenter", function (e) {
+        this.classList.add("drag-enter");
+        e.preventDefault();
+        return false;
+      });
+      el.bind("dragleave", function (e) {
+        this.classList.remove("drag-enter");
+        e.preventDefault();
+        return false;
+      });
+
+      el.bind("drop", function (e) {
+        e.preventDefault();
 
         $scope.file = e.dataTransfer.files[0];
         $scope.getFile();
+
         return false;
       });
 
     }
   }
-  
+
 })
