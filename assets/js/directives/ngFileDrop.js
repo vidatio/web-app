e5d44128fe0907edc08b5cc535d9028ee73c554c
MMTLukas <lwanko.mmt-m2014@fh-salzburg.ac.at>
2015-05-15 17:54:16 +0200

    feature: added drag-and-drop area

--- assets/js/directives/ngFileDrop.js
+++ assets/js/directives/ngFileDrop.js
@@ -1,6 +1,5 @@
 angular.module('vidatio').directive("ngFileDrop", function () {
 
-
   return {
     link: function ($scope, el) {
       //Drag-And-Drop Events:
@@ -9,33 +8,40 @@ angular.module('vidatio').directive("ngFileDrop", function () {
       //enter: cursor enters the drop-zone
       //drop: file got dropped on the drop-zone
       //drag, drag start and drag end isn't needed, because its only for DOM elements
-
       el.bind("dragover", function (e) {
-        this.classList.add("drag-enter");
         e.preventDefault();
         return false;
       });
       el.bind("dragenter", function (e) {
-        this.classList.add("drag-enter");
-        e.preventDefault();
-        return false;
-      });
-      el.bind("dragleave", function (e) {
-        this.classList.remove("drag-enter");
+        document.getElementsByClassName("drag-and-drop")[0].style.display = "block";
         e.preventDefault();
         return false;
       });
 
-      el.bind("drop", function (e) {
+      //Because the user which is dragging a file enters the body
+      //but instantly moving around or leaving the drag-and-drop area
+      //we have to bind the events to this drag-and-drop area
+      var dragDropArea = document.getElementsByClassName("drag-and-drop")[0];
+      dragDropArea.addEventListener("dragleave", function (e) {
+        dragDropArea.style.display = "none";
         e.preventDefault();
+        return false;
+      }, false);
+
+      dragDropArea.addEventListener("dragover", function (e) {
+        e.preventDefault();
+        return false;
+      }, false);
+
+      dragDropArea.addEventListener("drop", function (e) {
+        dragDropArea.style.display = "none";
 
         $scope.file = e.dataTransfer.files[0];
         $scope.getFile();
 
+        e.preventDefault();
         return false;
-      });
-
+      }, false);
     }
   }
-
-})
+});
